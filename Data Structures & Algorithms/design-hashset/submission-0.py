a7f96449f8fc286a697c31ad0d0c012bb9764e35
Abdulaziz <105636505+madaminovabdulaziz@users.noreply.github.com>
2026-04-16 17:54:23 +0500

    Add: design-hashset - submission-0

---
+++ Data Structures & Algorithms/design-hashset/submission-0.py
@@ -0,0 +1,31 @@
+class MyHashSet:
+
+    def __init__(self):
+        self.size = 10007
+        self.buckets = [[] for _ in range(self.size)] 
+
+    def _hash(self, key):
+        return key % self.size
+        
+        
+
+    def add(self, key: int) -> None:
+        index = self._hash(key)
+        bucket = self.buckets[index]
+        if key not in bucket:
+            bucket.append(key)
+
+        
+
+    def remove(self, key: int) -> None:
+        index = self._hash(key)
+        bucket = self.buckets[index]
+        if key in bucket:
+            bucket.remove(key)
+
+        
+
+    def contains(self, key: int) -> bool:
+        index = self._hash(key)
+        bucket = self.buckets[index]
+        return key in bucket
